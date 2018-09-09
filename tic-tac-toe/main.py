0134ace7ec081fa1dd9e98c573b4ce7b979d9df7
gstenger98 <gstenger98@gmail.com>
2018-09-09 02:10:20 -0700

    ai getting smarter, v0 and v1 done

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 BLANK = "_"
 
@@ -13,25 +14,27 @@ class Game:
 		return board
 
 	def printBoard(self):
-		for i in range(len(self.board)):
-			for j in range(len(self.board)):
-				print(self.board[i][j], end=" ")
+		for row in range(3):
+			for column in range(3):
+				print(self.board[row][column], end=" ")
 			print()
 
 	def play(self, player1, player2):
 		turn = player1
-		while not self.gameOver:
+		move_num = 0
+		while not self.gameOver and move_num != 9:
 			os.system('clear')
 			self.printBoard()
 
 			hasMoved = False
 			while not hasMoved:
 				decision = turn.move(self.board)
-				if self.move(decision, turn.color):
+				if self.move(decision, turn.letter):
 					hasMoved = True
 
 			if self.hasWon():
 				self.gameOver = True
+				break
 
 			# It's now the next player's turn
 			if turn == player1:
@@ -39,16 +42,19 @@ class Game:
 			else:
 				turn = player1
 
+			# Increment move number
+			move_num += 1
+
 		os.system('clear')
 		self.printBoard()
-		print(str(turn.color), "has won!")
+		if move_num == 9:
+			print("It's a tie!")
+		else:
+			print(str(turn.letter), "has won!")
 
-	def move(self, decision, color):
+	def move(self, decision, letter):
 		if self.board[(decision-1)//3][(decision-1)%3] == str(decision):
-			if color == "white":
-				self.board[(decision-1)//3][(decision-1)%3] = "X"
-			else:
-				self.board[(decision-1)//3][(decision-1)%3] = "O"
+			self.board[(decision-1)//3][(decision-1)%3] = letter
 			return True
 		else:
 			print("Spot has already been played")
@@ -56,30 +62,142 @@ class Game:
 
 	def hasWon(self):
 		for i in range(3):
-			if self.board[i][0] == self.board[i][1] == self.board[i][2] or self.board[0][i] == self.board[1][i] == self.board[2][i] or self.board[0][0] == self.board[1][1] == self.board[2][2] or self.board[2][0] == self.board[1][1] == self.board[0][2]:
+			if self.board[i][0] == self.board[i][1] == self.board[i][2]:
 				return True
-			else:
-				return False
-		self.gameOver == True
+			elif self.board[0][i] == self.board[1][i] == self.board[2][i]:
+				return True
+			elif self.board[0][0] == self.board[1][1] == self.board[2][2]:
+				return True
+			elif self.board[2][0] == self.board[1][1] == self.board[0][2]:
+				return True
+		else:
+			return False
 
-class HumanPlayer:
 
-	def __init__(self, color):
-		self.color = color
+class Player:
+
+	def __init__(self, letter):
+		self.letter = letter
+
+class HumanPlayer(Player):
+
+	def __init__(self, letter):
+		Player.__init__(self, letter)
 
 	def move(self, board):
 		decision = input("What move would you like to play? \n")
 		return int(decision)
 
-class ComputerPlayer:
+class ComputerPlayer(Player):
 
-	def __init__(self):
-		self.name = "computer"
+	def __init__(self, letter):
+		Player.__init__(self, letter)
+
+# This AI plays legal moves randomly
+class ComputerPlayerLevel0(ComputerPlayer):
+
+	def __init__(self, letter):
+		ComputerPlayer.__init__(self, letter)
+
+	def move(self, board):
+
+		# Find and store all open cells
+		open_cells = []
+		for row in range(3):
+			for col in range(3):
+				if board[row][col] == str((row)*3 + (col+1)):
+					open_cells.append((row)*3 + (col+1))
+
+		# Play a random legal move
+		return open_cells[random.randrange(len(open_cells))]
+
+
+# This AI can play immeditaly winning moves, but otherwise plays randomly
+class ComputerPlayerLevel1(ComputerPlayer):
+
+	def __init__(self, letter):
+		ComputerPlayer.__init__(self, letter)
+
+	def move(self, board):
+
+		# Find and store all open cells
+		open_cells = []
+		for row in range(3):
+			for col in range(3):
+				if board[row][col] == str((row)*3 + (col+1)):
+					open_cells.append((row)*3 + (col+1))
+
+		# Check if there are any immediately winning moves
+		for cell_num in open_cells:
+			print(cell_num)
+			row = (cell_num-1)//3
+			col = (cell_num-1)%3
+			if board[(row+1)%3][col] == board[(row+2)%3][col] == self.letter:
+				return cell_num
+			elif board[row][(col+1)%3] == board[row][(col+2)%3] == self.letter:
+				return cell_num
+			elif row == col and board[(row+1)%3][(col+1)%3] == board[(row+2)%3][(col+2)%3] == self.letter:
+				return cell_num
+			elif row + col == 2 and board[(row+1)%3][(col-1)%3] == board[(row-1)%3][(col+1)%3] == self.letter:
+				return cell_num
+
+		# Play a random legal move
+		return open_cells[random.randrange(len(open_cells))]
+
+# This AI will not play a move that allows an immediate win if there are alternatives
+# It will also play immediately winning moves
+# Otherwise it plays randomly
+class ComputerPlayerLevel2(ComputerPlayer):
+
+	def __init__(self, letter):
+		ComputerPlayer.__init__(self, letter)
+
+	def move(self, board):
+
+		# Find and store all open cells
+		open_cells = []
+		for row in range(3):
+			for col in range(3):
+				if board[row][col] == str((row)*3 + (col+1)):
+					open_cells.append((row)*3 + (col+1))
+
+		# Check if there are any immediately winning moves
+		for cell_num in open_cells:
+			row = (cell_num-1)//3
+			col = (cell_num-1)%3
+			if board[(row+1)%3][col] == board[(row+2)%3][col] == self.letter:
+				return cell_num
+			elif board[row][(col+1)%3] == board[row][(col+2)%3] == self.letter:
+				return cell_num
+			elif row == col and board[(row+1)%3][(col+1)%3] == board[(row+2)%3][(col+2)%3] == self.letter:
+				return cell_num
+			elif row + col == 2 and board[(row+1)%3][(col-1)%3] == board[(row-1)%3][(col+1)%3] == self.letter:
+				return cell_num
+
+		# Check if this move creates any immediately winning moves for the opponent
+
+		# Play a random legal move
+		return open_cells[random.randrange(len(open_cells))]
+
+# Checks if any board is winning (general function, will fix code structure soon)
+def isWinning(board):
+	for i in range(3):
+		if board[i][0] == board[i][1] == board[i][2]:
+			return True
+		elif board[0][i] == board[1][i] == board[2][i]:
+			return True
+		elif board[0][0] == board[1][1] == board[2][2]:
+			return True
+		elif board[2][0] == board[1][1] == board[0][2]:
+			return True
+	else:
+		return False
 
 def main():
 	game = Game()
-	player1 = HumanPlayer("white")
-	player2 = HumanPlayer("black")
+	player1 = HumanPlayer("X")
+	# player2 = HumanPlayer("black")
+	player2 = ComputerPlayerLevel2("O")
 	game.play(player1, player2)
 	# game.printBoard()
 
