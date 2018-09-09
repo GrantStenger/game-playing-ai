6c75089cb1b11fee5e1f532b0f91c94cf7b4f09f
gstenger98 <gstenger98@gmail.com>
2018-09-09 03:21:25 -0700

    ai's much better

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -120,33 +120,30 @@ class ComputerPlayerLevel1(ComputerPlayer):
 
 	def move(self, board):
 
-		# Find and store all open cells
+		# Find and store the cells of all legal moves
 		open_cells = []
 		for row in range(3):
 			for col in range(3):
 				if board[row][col] == str((row)*3 + (col+1)):
 					open_cells.append((row)*3 + (col+1))
 
-		# Check if there are any immediately winning moves
+		# If any of these moves result in an immediate win, do this. 
 		for cell_num in open_cells:
-			print(cell_num)
-			row = (cell_num-1)//3
-			col = (cell_num-1)%3
-			if board[(row+1)%3][col] == board[(row+2)%3][col] == self.letter:
-				return cell_num
-			elif board[row][(col+1)%3] == board[row][(col+2)%3] == self.letter:
-				return cell_num
-			elif row == col and board[(row+1)%3][(col+1)%3] == board[(row+2)%3][(col+2)%3] == self.letter:
-				return cell_num
-			elif row + col == 2 and board[(row+1)%3][(col-1)%3] == board[(row-1)%3][(col+1)%3] == self.letter:
+
+			# Make a copy of the board (do not want to pass by reference)
+			new_board = [row[:] for row in board]
+
+			# Check if executing this move will result in a win
+			new_board = updateBoard(new_board, cell_num, self.letter)
+			if isWinning(new_board, self.letter):
+				# If so, execute this move
 				return cell_num
 
 		# Play a random legal move
 		return open_cells[random.randrange(len(open_cells))]
 
-# This AI will not play a move that allows an immediate win if there are alternatives
-# It will also play immediately winning moves
-# Otherwise it plays randomly
+# This AI blocks the opponent from playing immediately winning moves
+# It will also play immediately winning moves for itself, otherwise it plays randomly
 class ComputerPlayerLevel2(ComputerPlayer):
 
 	def __init__(self, letter):
@@ -161,46 +158,75 @@ class ComputerPlayerLevel2(ComputerPlayer):
 				if board[row][col] == str((row)*3 + (col+1)):
 					open_cells.append((row)*3 + (col+1))
 
-		# Check if there are any immediately winning moves
+		# If any of these moves result in an immediate win, do this. 
 		for cell_num in open_cells:
-			row = (cell_num-1)//3
-			col = (cell_num-1)%3
-			if board[(row+1)%3][col] == board[(row+2)%3][col] == self.letter:
-				return cell_num
-			elif board[row][(col+1)%3] == board[row][(col+2)%3] == self.letter:
-				return cell_num
-			elif row == col and board[(row+1)%3][(col+1)%3] == board[(row+2)%3][(col+2)%3] == self.letter:
-				return cell_num
-			elif row + col == 2 and board[(row+1)%3][(col-1)%3] == board[(row-1)%3][(col+1)%3] == self.letter:
+
+			# Make a copy of the board (do not want to pass by reference)
+			new_board = [row[:] for row in board]
+
+			# Check if executing this move will result in a win
+			new_board = updateBoard(new_board, cell_num, self.letter)
+			if isWinning(new_board, self.letter):
+				# If so, execute this move
 				return cell_num
 
 		# Check if this move creates any immediately winning moves for the opponent
+		for cell_num in open_cells:
+
+			# Create the new board that would result in making this legal move
+			new_board = [row[:] for row in board]
+			new_board = updateBoard(new_board, cell_num, self.letter)
+
+			# Iterate through each of the opponents move, an if they could win, block them.
+			opponent_open_cells = []
+			for row in range(3):
+				for col in range(3):
+					if new_board[row][col] == str((row)*3 + (col+1)):
+						opponent_open_cells.append((row)*3 + (col+1))
+			for opponent_cell_num in opponent_open_cells:
+				opponent_new_board = [row[:] for row in new_board]
+				opponent_new_board = updateBoard(opponent_new_board, opponent_cell_num, otherLetter(self.letter))
+				if isWinning(opponent_new_board, otherLetter(self.letter)):
+					return opponent_cell_num
 
 		# Play a random legal move
 		return open_cells[random.randrange(len(open_cells))]
 
 # Checks if any board is winning (general function, will fix code structure soon)
-def isWinning(board):
+def isWinning(board, letter):
 	for i in range(3):
-		if board[i][0] == board[i][1] == board[i][2]:
+		if board[i][0] == board[i][1] == board[i][2] == letter:
 			return True
-		elif board[0][i] == board[1][i] == board[2][i]:
+		elif board[0][i] == board[1][i] == board[2][i] == letter:
 			return True
-		elif board[0][0] == board[1][1] == board[2][2]:
+		elif board[0][0] == board[1][1] == board[2][2] == letter:
 			return True
-		elif board[2][0] == board[1][1] == board[0][2]:
+		elif board[2][0] == board[1][1] == board[0][2] == letter:
 			return True
 	else:
 		return False
 
+def updateBoard(board, cell_num, letter):
+	board[(cell_num-1)//3][(cell_num-1)%3] = letter
+	return board
+
+def PrintBBoard(board):
+	for row in range(3):
+		for column in range(3):
+			print(board[row][column], end=" ")
+		print()
+
+def otherLetter(letter):
+	if letter == "X":
+		return "O"
+	else:
+		return "X"
+
 def main():
 	game = Game()
 	player1 = HumanPlayer("X")
-	# player2 = HumanPlayer("black")
 	player2 = ComputerPlayerLevel2("O")
 	game.play(player1, player2)
-	# game.printBoard()
-
 
 if __name__ == "__main__":
 	main()
